dea944040f289b459588304218a53796bb28ecf3
Kevin Cho <kevc.pro@gmail.com>
2019-01-12 23:07:27 -0500

    Initial Application

--- app/src/main/java/net/kboy/snapkitclient/MainActivity.kt
+++ app/src/main/java/net/kboy/snapkitclient/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity(), LoginStateController.OnLoginStateChang
         setContentView(R.layout.activity_main)
 
         val label: TextView = findViewById(R.id.textView)
-        label.text = "Pemoji"
+        label.text = "Snapchat Sucks"
 
         SnapLogin.getLoginStateController(this).addOnLoginStateChangedListener(this)
 
